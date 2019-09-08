8160380fca63da459b9135f92453150dc8d5aefd
eimantaslilia <eimantaslilia@gmail.com>
2019-09-08 23:35:59 +0100

    tdd-by-example

---
+++ src/main/java/guru/springframework/Bank.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import java.util.HashMap;
+
+class Bank {
+
+    private final HashMap<Pair, Integer> rateMap = new HashMap<>();
+
+    Money reduce(Expression source){
+        return source.reduce(this, "USD");
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
+    }
+
+    public int rate(String from, String to) {
+        if(from.equals(to)){
+            return 1;
+        }
+        return rateMap.get(new Pair(from, to));
+    }
+}
---
+++ src/main/java/guru/springframework/Expression.java
@@ -0,0 +1,12 @@
+package guru.springframework;
+
+public interface Expression {
+
+    Money reduce(Bank bank, String to);
+
+    Expression plus(Expression addend);
+
+    Expression times(int multiplier);
+
+
+}
